e8464bfb5bd4bc333a6d893b77af3381ca3539c7
Asma2442 <70644511+Asma2442@users.noreply.github.com>
2020-10-05 03:06:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,24 +7,14 @@ function setup() {
 
   speed=random(223,321)
   weight=random(30,52)
-
-
   thickness=random(22,83)
 
+  bullet=createSprite(50, 200, 50,5);  
+  bullet.velocityX = speed;
+  bullet.shapeColor=color(255);
 
-
-
-  
-    bullet=createSprite(50, 200, 50,5);  
-    bullet.velocityX = speed;
-    bullet.shapeColor=color(255);
-
- 
-  	
-    
-    wall=createSprite(1200, 200, thickness, height/2);  
-
-    wall.shapeColor=color(230,230,230);
+  wall=createSprite(1200, 200, thickness, height/2);  
+  wall.shapeColor=color(230,230,230);
 }
 
 
@@ -38,7 +28,7 @@ function draw() {
   	
 	if(damage>10)
 	{
-		wall.shapeColor=color("red");
+		wall.shapeColor="red";
 		
 	}
 
@@ -46,7 +36,7 @@ function draw() {
 
 	if(damage<10)
 	{
-		wall.shapeColor=color("green");
+		wall.shapeColor="green";
 	}
 	
   }
